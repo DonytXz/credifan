7fde94cc06e906161dad013376241112c2378deb
chatblack1999 <chatblack1999@gmail.com>
2021-10-22 10:26:46 -0500

    [UPDATED] responsive home

--- src/components/Landing/Home.jsx
+++ src/components/Landing/Home.jsx
@@ -7,19 +7,28 @@ const Home = () => {
   return (
     <>
       <Header />
-      <div className="flex flex-col lg:flex-row ">
+      <div className="flex flex-col lg:flex-row w-10/12 mx-auto">
         <div>
           <img className="mx-auto" src={Hero} alt="" srcSet="" />
         </div>
         <div className="flex flex-col justify-center">
-          <h1 className="text-center text-xl font-bold text-blue-regular">hola casa!</h1>
-          <img className="mx-auto" src={Logo} alt="" srcSet="" />
-          <span className="text-center uppercase text-lg text-blue-regular">transforma tu vida</span>
-          <p className="w-10/12 mx-auto text-blue-regular">
+          <div className="flex flex-col md:flex-row mx-auto">
+            <h1 className="text-center text-4xl font-bold text-blue-regular mr-6">
+              hola casa!
+            </h1>
+            <img className="" src={Logo} alt="" srcSet="" />
+          </div>
+
+          <span className="text-center uppercase text-2xl text-blue-regular">
+            transforma tu vida
+          </span>
+          <p className="w-10/12 mx-auto text-lg text-blue-regular">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
             nobis recusandae perspiciatis dolorem fugiat cum commodi!
           </p>
-          <button className="w-3/4 mx-auto uppercase p-6 bg-blue-light text-white">cotiza ahora!</button>
+          <button className="w-3/4 mx-auto uppercase p-6 bg-blue-light text-white">
+            cotiza ahora!
+          </button>
         </div>
       </div>
     </>
